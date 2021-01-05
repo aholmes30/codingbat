6a6640f3a3306409ba87756457d5f79413345473
Anna <amholmes1300@gmail.com>
2021-01-05 17:32:49 -0500

    added postneg codingbat

--- src/warmup1/NearHundred.java
+++ src/warmup1/NearHundred.java
@@ -1,4 +1,26 @@
 package warmup1;
 
 public class NearHundred {
+
+    public boolean nearHundred(int n) {
+
+        //return data: boolean
+        //param: int
+
+        if(Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10){
+            return true;
+        }
+        return false;
+    }
+
 }
+
+/*
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num)
+computes the absolute value of a number.
+
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+ */
---
+++ src/warmup1/PosNeg.java
@@ -0,0 +1,30 @@
+package warmup1;
+
+public class PosNeg {
+
+    public boolean posNeg(int a, int b, boolean negative) {
+
+        //return data: boolean
+        //param: int, boolean
+
+        if(negative) {
+            return a < 0 && b < 0;
+        }
+        if(a < 0 && b > 0 || a > 0 && b < 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
+
+/*
+Given 2 int values, return true if one is negative and one is positive.
+Except if the parameter "negative" is true, then return true only if both are negative.
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+ */
