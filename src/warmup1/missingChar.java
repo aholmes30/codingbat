b1eafdf98cae687bda0ba9a9c428cd6c8a346581
Anna <amholmes1300@gmail.com>
2021-01-05 17:43:29 -0500

    added missing char codingbat

---
+++ src/warmup1/missingChar.java
@@ -0,0 +1,24 @@
+package warmup1;
+
+public class missingChar {
+
+    public String missingChar(String str, int n) {
+        // return data: String
+        // param: str, int
+
+        return str.substring(0, n) + str.substring(n+1);
+        // start at 0, then go to n and ADD from n to + 1
+
+    }
+
+}
+
+/*
+Given a non-empty string and an int n, return a new string where the char at
+index n has been removed. The value of n will be a valid index of a char in the
+original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn"
+ */
