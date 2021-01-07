81e5382c4acf3ae46988068f5c729f75a26c8f8a
Anna <amholmes1300@gmail.com>
2021-01-07 17:25:48 -0500

    added codingbat front22 problen

---
+++ src/warmup1/front22.java
@@ -0,0 +1,39 @@
+package warmup1;
+
+public class front22 {
+
+    public String front22(String str) {
+
+        //return data: String
+        //param: String
+
+        if(str.length() < 2) {
+            return str.substring(0) + str.substring(0) + str.substring(0);
+        }
+
+        //get the first two chars:
+
+        String firstTwo = str.substring(0, 2);
+
+        String result = "";
+
+        result = result + firstTwo + str + firstTwo;
+
+        return result;
+
+
+    }
+
+}
+
+/*
+
+Given a string, take the first 2 chars and return the string with the 2 chars added
+at both the front and back, so "kitten" yields"kikittenki". If the string length is
+less than 2, use whatever chars are there.
+
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+ */
