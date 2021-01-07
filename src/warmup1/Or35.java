d24ce7cbcd34aa6d950b2cb304b0360424df264e
Anna <amholmes1300@gmail.com>
2021-01-07 17:24:29 -0500

    updated codingbat Or35

--- src/warmup1/Or35.java
+++ src/warmup1/Or35.java
@@ -1,6 +1,6 @@
 package warmup1;
 
-public class or35 {
+public class Or35 {
 
     public static boolean or35(int n) {
 
@@ -9,6 +9,11 @@ public class or35 {
 
         return n % 3 == 0 || n % 5 == 0;
 
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println((or35(8)));
     }
 
 
