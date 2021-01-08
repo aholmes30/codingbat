0c08ff84aea3dbb0fd6ac912bd2b02471e35fb67
Anna <amholmes1300@gmail.com>
2021-01-08 18:54:44 -0500

    added codingbat problem In1020

---
+++ src/warmup1/In1020.java
@@ -0,0 +1,24 @@
+package warmup1;
+
+public class In1020 {
+
+    public boolean in1020(int a, int b) {
+
+        //return data: boolean
+        //param: int
+
+        return a >= 10 && a <= 20 || a >= 10 && b <= 20;
+
+    }
+
+
+
+}
+
+/*
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+in1020(12, 99) → true
+in1020(21, 12) → true
+in1020(8, 99) → false
+ */
