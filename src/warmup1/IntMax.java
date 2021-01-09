0c65fd32585644e55ec6edd6dcb2bda6162c3852
Anna <amholmes1300@gmail.com>
2021-01-09 11:25:58 -0500

    added codingbat IntMax problem

---
+++ src/warmup1/IntMax.java
@@ -0,0 +1,24 @@
+package warmup1;
+
+public class IntMax {
+    public static int intMax(int a, int b, int c) {
+
+        if(a > b && a > c) {
+            return a;
+        }
+        if(b > a && b > c) {
+            return b;
+        }
+        return c;
+    }
+
+
+}
+
+/*
+Given three int values, a b c, return the largest.
+
+intMax(1, 2, 3) → 3
+intMax(1, 3, 2) → 3
+intMax(3, 2, 1) → 3
+ */
