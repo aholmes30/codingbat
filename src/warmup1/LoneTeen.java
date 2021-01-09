6216a6dc926dcba2e29a9f9a67c6fcf11a2aa966
Anna <amholmes1300@gmail.com>
2021-01-08 19:09:11 -0500

    added codingbat problem LoneTeen

---
+++ src/warmup1/LoneTeen.java
@@ -0,0 +1,29 @@
+package warmup1;
+
+public class LoneTeen {
+
+    public boolean loneTeen(int a, int b) {
+
+        // return data: boolean
+        // param: int
+
+        //if one or the other is teen BUT not both
+
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen & bTeen);
+
+    }
+
+}
+
+/*
+
+We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+Given 2 int values, return true if one or the other is teen, but not both.
+
+loneTeen(13, 99) → true
+loneTeen(21, 19) → true
+loneTeen(13, 13) → false
+ */
