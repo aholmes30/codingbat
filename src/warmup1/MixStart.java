0e329d3697bb1e16bcd77cd5579558cf714237d8
Anna <amholmes1300@gmail.com>
2021-01-09 11:09:47 -0500

    added MixStart codingbat problem

---
+++ src/warmup1/MixStart.java
@@ -0,0 +1,26 @@
+package warmup1;
+
+public class MixStart {
+    public static boolean mixStart(String str) {
+
+        // return data: boolean
+        // param: String
+
+        if(str.length() >= 3 && str.substring(1, 3).equals("ix")){
+            return true;
+        }
+        return false;
+    }
+
+
+}
+
+/*
+
+Return true if the given string begins with "mix", except the 'm'
+can be anything, so "pix", "9ix" .. all count.
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false
+ */
