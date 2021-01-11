54e41880388d85810906da48fb8b6ec95d0b57d6
Anna <amholmes1300@gmail.com>
2021-01-11 16:56:54 -0500

    added codingbat LastDigit problem

---
+++ src/warmup1/LastDigit.java
@@ -0,0 +1,25 @@
+package warmup1;
+
+public class LastDigit {
+
+    public boolean lastDigit(int a, int b) {
+
+        // return data: boolean
+        // param: int
+
+        return a % 10 == b % 10;
+
+    }
+
+
+
+}
+
+/*
+Given two non-negative int values, return true if they have the same last digit,
+such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+lastDigit(7, 17) → true
+lastDigit(6, 17) → false
+lastDigit(3, 113) → true
+ */
