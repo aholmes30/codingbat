853afa9fda90592b9283b0b9a7ceda31d6b8101d
Anna <amholmes1300@gmail.com>
2021-01-11 17:01:45 -0500

    added EveryNth codingbat problem

---
+++ src/warmup1/EveryNth.java
@@ -0,0 +1,30 @@
+package warmup1;
+
+public class EveryNth {
+
+    public String everyNth(String str, int n) {
+
+        // return data: String
+        // param: String, n
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i = i + n){
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+
+
+}
+
+/*
+Given a non-empty string and an int N, return the string made starting with char 0,
+and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ...
+and so on. N is 1 or more.
+
+everyNth("Miracle", 2) → "Mrce"
+everyNth("abcdefg", 2) → "aceg"
+everyNth("abcdefg", 3) → "adg"
+ */
