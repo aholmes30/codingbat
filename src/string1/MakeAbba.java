67edd3fa22418b57aec13cac3b34b563d7715ca1
Anna <amholmes1300@gmail.com>
2021-01-12 09:58:34 -0500

    added codingbat MakeAbba problem

---
+++ src/string1/MakeAbba.java
@@ -0,0 +1,24 @@
+package string1;
+
+public class MakeAbba {
+    public static String makeAbba(String a, String b) {
+
+        //return data: String
+        //param: String
+
+        return a + b + b + a;
+
+    }
+
+
+}
+
+/*
+
+Given two strings, a and b, return the result of putting them together
+in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+ */
