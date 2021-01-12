8a51152e6c04afd912625a26d99e2140824e486c
Anna <amholmes1300@gmail.com>
2021-01-12 09:56:45 -0500

    added codingbat problem HelloName

---
+++ src/string1/HelloName.java
@@ -0,0 +1,24 @@
+package string1;
+
+public class HelloName {
+
+    public static String helloName(String name) {
+
+        //return data: String
+        //param: String
+
+        return "Hello" + " " + name + "!";
+
+    }
+
+
+}
+
+/*
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+ */
