3dc6844681f50e234cc0168d3a0ffac9d04cfe8e
Anna <amholmes1300@gmail.com>
2021-01-15 17:47:20 -0500

    updated NonStart codingbat

--- src/string1/NonStart.java
+++ src/string1/NonStart.java
@@ -2,13 +2,18 @@ package string1;
 
 public class NonStart {
 
-    public String nonStart(String a, String b) {
+    public static String nonStart(String a, String b) {
 
         //return data: String
         //param: String
 
         return a.substring(1) + b.substring(1);
 
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(nonStart("Good Morning", "Audrey"));
     }
 
 
