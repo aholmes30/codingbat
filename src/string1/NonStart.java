56e4551c18178d7cf1e9cd71e438f8bf66f79ad3
Anna <amholmes1300@gmail.com>
2021-01-15 17:46:33 -0500

    added NonStart problem codingbat

---
+++ src/string1/NonStart.java
@@ -0,0 +1,25 @@
+package string1;
+
+public class NonStart {
+
+    public String nonStart(String a, String b) {
+
+        //return data: String
+        //param: String
+
+        return a.substring(1) + b.substring(1);
+
+    }
+
+
+}
+
+/*
+
+Given 2 strings, return their concatenation, except omit the first char of each.
+The strings will be at least length 1.
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+ */
