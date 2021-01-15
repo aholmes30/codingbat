a54df6c67a56090eacc2b18752f3986e69d3ac79
Anna <amholmes1300@gmail.com>
2021-01-15 17:54:23 -0500

    added codingbat TheEnd problem

---
+++ src/string1/TheEnd.java
@@ -0,0 +1,30 @@
+package string1;
+
+public class TheEnd {
+
+    public static String theEnd(String str, boolean front) {
+
+        //return data: String
+        //param: String boolean
+
+        if(front){
+            return str.substring(0, 1); //just return string length 1 from front
+        }
+
+        return str.substring(str.length()-1);  //return string length 1 from back
+
+    }
+
+
+
+}
+
+/*
+
+Given a string, return a string length 1 from its front, unless front is false,
+in which case return a string length 1 from its back. The string will be non-empty.
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o"
+ */
