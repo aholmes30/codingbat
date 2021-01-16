2a0a80eee8b8b9fe964c6e1cc0bca7b80f373ee2
Anna <amholmes1300@gmail.com>
2021-01-16 11:25:45 -0500

    added codingbat MiddleTwo

---
+++ src/string1/MiddleTwo.java
@@ -0,0 +1,30 @@
+package string1;
+
+public class MiddleTwo {
+
+    public String middleTwo(String str) {
+
+        //return data: String
+        //param: String
+
+        int middle = str.length() / 2;
+
+
+        return str.substring(middle - 1, middle + 1);
+
+
+    }
+
+
+
+}
+
+/*
+
+Given a string of even length, return a string made of the middle two chars,
+so the string "string" yields "ri". The string length will be at least 2.
+
+middleTwo("string") → "ri"
+middleTwo("code") → "od"
+middleTwo("Practice") → "ct"
+ */
