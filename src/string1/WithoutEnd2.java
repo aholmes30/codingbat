5a7a1bf2a71c35d17db1adbaa551f37fbee15336
Anna <amholmes1300@gmail.com>
2021-01-16 11:24:15 -0500

    added WithoutEnd2 codingbat problem

---
+++ src/string1/WithoutEnd2.java
@@ -0,0 +1,32 @@
+package string1;
+
+public class WithoutEnd2 {
+
+    public String withouEnd2(String str) {
+
+        //return data: String
+        //param: String
+
+        // first char would be: str.substring(0, 1);
+        // last char would be: str.substring(str.lengnth()-1);
+
+
+
+        if(str.length() <= 1){
+            return "";
+        }
+        return str.substring(1, str.length()-1);
+
+    }
+
+}
+
+/*
+
+Given a string, return a version without both the first and last char of the string.
+The string may be any length, including 0.
+
+withouEnd2("Hello") → "ell"
+withouEnd2("abc") → "b"
+withouEnd2("ab") → ""
+ */
