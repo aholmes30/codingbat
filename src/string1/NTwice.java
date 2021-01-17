80de0e5d5b9aee669011f9ef98f27205746b3467
Anna <amholmes1300@gmail.com>
2021-01-17 10:43:37 -0500

    updated NTwice codingbat

--- src/string1/NTwice.java
+++ src/string1/NTwice.java
@@ -6,22 +6,17 @@ public class NTwice {
         // return data: String
         // param: String, int
 
-        return str.substring(0, n) + str.substring(str.length()-n);
+        return str.substring(0, n) + str.substring(str.length() - n);
 
     }
 
-/* nTwice("Hello", 2) → "Helo" --> He + lo (n = 2)
-...so str.substring(0, n) + str.substring(str.length()-n)
-
-
-
-*/
-
+    public static void main(String[] args) {
+        System.out.println(nTwice("kitty", 5));
+    }
 
 }
 
 /*
-
 Given a string and an int n, return a string made of the first and last
 n chars from the string. The string length will be at least n.
 
