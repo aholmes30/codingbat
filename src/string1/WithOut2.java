46af637012d5648ebdb994d8dd72ab95d7a656a1
Anna <amholmes1300@gmail.com>
2021-01-20 11:12:50 -0500

    added Without2 codingbat problem

---
+++ src/string1/WithOut2.java
@@ -0,0 +1,36 @@
+package string1;
+
+public class WithOut2 {
+
+    public static String without2(String str) {
+
+        // return data: String
+        // param: String
+
+        // if substring length two is same at beg and end -->
+        // str.substring(0, 2) = str.substring(str.length()-2, str.length()-1)
+
+        if(str.length() == 2) {
+            return "";
+        }
+        if(str.length() < 2){
+            return str;
+        }
+        if(str.substring(0, 2).equals(str.substring(str.length()-2, str.length())))
+            return str.substring(2, str.length());
+
+        else return str;
+    }
+
+
+}
+
+/*
+Given a string, if a length 2 substring appears at both its beginning and end,
+return a string without the substring at the beginning, so "HelloHe" yields "lloHe".
+The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") →
+ */
