2711c4398eb808973618704afa14bf14fdb6fc99
Anna <amholmes1300@gmail.com>
2021-01-23 14:11:19 -0500

    added SortaSum codinbat problem

---
+++ src/logic1/SortaSum.java
@@ -0,0 +1,27 @@
+package logic1;
+
+public class SortaSum {
+
+    public static int sortaSum(int a, int b) {
+
+        // return data: int
+        // param: int
+
+        if(a + b >= 10 && a + b <= 19) {
+            return 20;
+        }
+        return a + b;
+    }
+
+
+}
+
+/*
+
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19
+inclusive, are forbidden, so in that case just return 20.
+
+sortaSum(3, 4) → 7
+sortaSum(9, 4) → 20
+sortaSum(10, 11) → 21
+ */
