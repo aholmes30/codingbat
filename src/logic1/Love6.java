f897dc7d422d786deb3c852e3eac0eebc949fb91
Anna <amholmes1300@gmail.com>
2021-01-23 14:15:48 -0500

    added Love6 codingbat problem

---
+++ src/logic1/Love6.java
@@ -0,0 +1,31 @@
+package logic1;
+
+public class Love6 {
+    public static boolean love6(int a, int b) {
+
+        // return data: boolean
+        // param: int
+
+        if(a == 6 || b == 6) {
+            return true;
+        }
+        if(a + b == 6) {
+            return true;
+        }
+        if(Math.abs(a - b) == 6) {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+/*
+The number 6 is a truly great number. Given two int values, a and b, return
+true if either one is 6. Or if their sum or difference is 6. Note: the function
+Math.abs(num) computes the absolute value of a number.
+
+love6(6, 4) → true
+love6(4, 5) → false
+love6(1, 5) → true
+ */
