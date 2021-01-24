18950b55c8778f61b96604af8169c4b8cb5dd2f5
Anna <amholmes1300@gmail.com>
2021-01-24 16:25:03 -0500

    added SpecialEleven codingbat problem

---
+++ src/logic1/SpecialEleven.java
@@ -0,0 +1,28 @@
+package logic1;
+
+public class SpecialEleven {
+
+    public static boolean specialEleven(int n) {
+
+        // return data: boolean
+        // param: int
+
+        if(n % 11 == 0 || n % 11 == 0 + 1) {
+            return true;
+        }
+        return false;
+
+    }
+
+}
+
+/*
+We'll say a number is special if it is a multiple of 11 or if it is one more than a
+multiple of 11. Return true if the given non-negative number is special.
+Use the % "mod" operator -- see Introduction to Mod
+
+
+specialEleven(22) → true
+specialEleven(23) → true
+specialEleven(24) → false
+ */
