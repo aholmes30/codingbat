4c7838cc859bf7cdd5b2a69972ecb028bf264c3f
Anna <amholmes1300@gmail.com>
2021-01-24 16:26:48 -0500

    added More20 Codingbat problem

---
+++ src/logic1/More20.java
@@ -0,0 +1,27 @@
+package logic1;
+
+public class More20 {
+
+    public static boolean more20(int n) {
+
+        // return data: boolean
+        // param: int
+
+        return (n % 20 == 0 + 1 || n % 20 == 0 + 2);
+
+    }
+
+
+
+}
+
+/*
+
+Return true if the given non-negative number is 1 or 2 more than a
+multiple of 20. See also: Introduction to Mod
+
+
+more20(20) → false
+more20(21) → true
+more20(22) → true
+ */
