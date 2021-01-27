3adfa5203433fe81ae68176dc7727252f1b95506
Anna <amholmes1300@gmail.com>
2021-01-26 20:24:12 -0500

    updated AnswerCell codingbat

--- src/logic1/AnswerCell.java
+++ src/logic1/AnswerCell.java
@@ -16,6 +16,11 @@ public class AnswerCell {
         return false;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(answerCell(false,  true, false));
+    }
+
 
 
 }
