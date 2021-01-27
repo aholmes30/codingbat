def81e9fcc0c9191443caf43a1861f7fbbb628bd
Anna <amholmes1300@gmail.com>
2021-01-26 20:36:07 -0500

    added TwoAsOne codingbat

---
+++ src/logic1/TwoAsOne.java
@@ -0,0 +1,24 @@
+package logic1;
+
+public class TwoAsOne {
+
+    public static boolean twoAsOne(int a, int b, int c) {
+
+        // return data: boolean
+        // param: int
+
+        return (a + b == c) || (a + c == b) || (c + b == a);
+
+    }
+
+
+}
+
+/*
+Given three ints, a b c, return true if it is possible to add two of
+the ints to get the third.
+
+twoAsOne(1, 2, 3) → true
+twoAsOne(3, 1, 2) → true
+twoAsOne(3, 2, 2) → false
+ */
