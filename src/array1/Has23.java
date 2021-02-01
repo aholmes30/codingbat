ecc852009c10e26d70c89cf3dad5b43143c5e7e7
Anna <amholmes1300@gmail.com>
2021-02-01 10:16:50 -0500

    added Has23 codingbat

---
+++ src/array1/Has23.java
@@ -0,0 +1,27 @@
+package array1;
+
+public class Has23 {
+
+    public static boolean has23(int[] nums) {
+
+        // return data: boolean
+        // param: int
+
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 2 || nums[i] == 3)
+                return true;
+        }
+        return false;
+    }
+
+}
+
+/*
+
+Given an int array length 2, return true if it contains a 2 or a 3.
+
+has23([2, 5]) → true
+has23([4, 3]) → true
+has23([4, 5]) → false
+ */
