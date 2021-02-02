c5cbe3129a70b5ef4859d1315eb3f9027263ead6
Anna <amholmes1300@gmail.com>
2021-02-02 09:59:04 -0500

    updated No23 codingbat

--- src/array1/No23.java
+++ src/array1/No23.java
@@ -8,8 +8,11 @@ public class No23 {
         // param: int[] nums
 
         return nums[0] != 2 && nums[1] != 2 && nums[0] != 3 && nums[1] != 3;
+    }
 
-
+    public static void main(String[] args) {
+        int[] nums = {5, 34};
+        System.out.println(no23(nums));
     }
 }
 
