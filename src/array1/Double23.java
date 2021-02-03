77470ac595ae5b9625b409185c66268f72a4e72b
Anna <amholmes1300@gmail.com>
2021-02-03 18:39:12 -0500

    added Double23 codingbat

---
+++ src/array1/Double23.java
@@ -0,0 +1,28 @@
+package array1;
+
+public class Double23 {
+    public static boolean double23(int[] nums) {
+
+        // return data: boolean
+        // param: int[]
+
+        if(nums.length < 2) {
+            return false;
+        }
+        if(nums[0] == 2 && nums[1] == 2 || nums[0] == 3 && nums[1] == 3){
+            return true;
+        }
+        return false;
+    }
+
+
+}
+
+/*
+Given an int array, return true if the array contains 2 twice, or 3 twice.
+The array will be length 0, 1, or 2.
+
+double23([2, 2]) → true
+double23([3, 3]) → true
+double23([2, 3]) → false
+ */
