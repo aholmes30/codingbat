6485e5e6dbddfd240d9f736eb8318d133efd359e
Anna <amholmes1300@gmail.com>
2021-02-03 18:43:35 -0500

    added Start1 codingbat

---
+++ src/array1/Start1.java
@@ -0,0 +1,30 @@
+package array1;
+
+public class Start1 {
+    public static int start1(int[] a, int[] b) {
+
+        // return data: int
+        // param = int[]
+
+        int count = 0;
+
+        if(a.length > 0 && a[0] == 1){
+            count++;
+        }
+        if(b.length > 0 && b[0] == 1){
+            count++;
+        }
+        return count;
+    }
+
+
+}
+
+/*
+Start with 2 int arrays, a and b, of any length.
+Return how many of the arrays have 1 as their first element.
+
+start1([1, 2, 3], [1, 3]) → 2
+start1([7, 2, 3], [1]) → 1
+start1([1, 2], []) → 1
+ */
