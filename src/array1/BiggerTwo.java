397d21b54c0435a2df22ed721d4572f6a9fa224b
Anna <amholmes1300@gmail.com>
2021-02-04 17:54:35 -0500

    added BiggerTwo codingbat problem

---
+++ src/array1/BiggerTwo.java
@@ -0,0 +1,29 @@
+package array1;
+
+public class BiggerTwo {
+    public static int[] biggerTwo(int[] a, int[] b) {
+
+        // return data: int[]
+        // param: int[]
+
+
+        if(a[0] + a[1] == b[0] + b[1]){
+            return a;
+        }
+        if(a[0] + a[1] > b[0] + b[1]) {
+            return a;
+        }
+        else return b;
+    }
+
+}
+
+/*
+
+Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each
+array. Return the array which has the largest sum. In event of a tie, return a.
+
+biggerTwo([1, 2], [3, 4]) → [3, 4]
+biggerTwo([3, 4], [1, 2]) → [3, 4]
+biggerTwo([1, 1], [1, 2]) → [1, 2]
+ */
