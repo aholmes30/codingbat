1b698f7352deba1f3610982032cc4d257402fda3
Anna <amholmes1300@gmail.com>
2021-02-09 11:02:09 -0500

    added ArrayCount9 codingbat

---
+++ src/warmup2/ArrayCount9.java
@@ -0,0 +1,27 @@
+package warmup2;
+
+public class ArrayCount9 {
+
+    public static int arrayCount9(int[] nums) {
+
+        // return data: int
+        // param: int[]
+
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 9)
+                count++;
+        }
+        return count;
+    }
+
+}
+
+/*
+Given an array of ints, return the number of 9's in the array.
+
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+arrayCount9([1, 9, 9, 3, 9]) → 3
+ */
