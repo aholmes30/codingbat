46c02d55cef0e12b246e1e04e5245aba7c258db9
Anna <amholmes1300@gmail.com>
2021-02-09 10:58:43 -0500

    added Last2 codingbat problem

---
+++ src/warmup2/Last2.java
@@ -0,0 +1,36 @@
+package warmup2;
+
+public class Last2 {
+    public static int last2(String str) {
+
+        // return data: int
+        // param: String
+
+        // find the last two chars of string
+        // find out how many times those last two chars repeat
+        // last two in string: str.substring(str.length()-2)
+
+        int count = 0;
+        if(str.length() > 2) {
+            String lastTwoChars = str.substring(str.length()-2);
+            for(int i = 0; i < str.length()-2; i++) {
+                if(str.substring(i, i + 2).equals(lastTwoChars)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
+
+/*
+
+Given a string, return the count of the number of times that a substring length 2
+appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1
+(we won't count the end substring).
+
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+last2("axxxaaxx") → 2
+ */
