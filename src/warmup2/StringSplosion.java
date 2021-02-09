50f1f1db7774966c69ac8c7ebd176fa02bcd2e55
Anna <amholmes1300@gmail.com>
2021-02-09 10:56:48 -0500

    added StringSplosion codingbat problem

---
+++ src/warmup2/StringSplosion.java
@@ -0,0 +1,26 @@
+package warmup2;
+
+public class StringSplosion {
+    public static  String stringSplosion(String str) {
+
+        // return data: String
+        // param: String
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++) {
+            result = result + str.substring(0, i+1);  // every iteration add from 0 to i + 1
+        }
+        return result;
+
+    }
+}
+
+/*
+
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+stringSplosion("Code") → "CCoCodCode"
+stringSplosion("abc") → "aababc"
+stringSplosion("ab") → "aab"
+ */
