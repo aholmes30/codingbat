d1209c6e034a3774d87f84f66e47ebcc2e2e6ea9
Anna <amholmes1300@gmail.com>
2021-02-14 12:26:01 -0500

    added StringBits codingbat

---
+++ src/warmup2/StringBits.java
@@ -0,0 +1,28 @@
+package warmup2;
+
+public class StringBits {
+
+    public static String stringBits(String str) {
+
+        // return data: String
+        // param: String
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i = i + 2) { // we need to increment by two, not 1
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+}
+
+/*
+
+Given a string, return a new string made of every other char starting with the first,
+so "Hello" yields "Hlo".
+
+stringBits("Hello") → "Hlo"
+stringBits("Hi") → "H"
+stringBits("Heeololeo") → "Hello"
+ */
