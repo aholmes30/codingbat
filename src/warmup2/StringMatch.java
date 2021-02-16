23f9acffb07c307b67c1fbd343fd5558b9e865bf
Anna <amholmes1300@gmail.com>
2021-02-15 19:51:52 -0500

    added StringMatch codingbat

---
+++ src/warmup2/StringMatch.java
@@ -0,0 +1,40 @@
+package warmup2;
+
+public class StringMatch {
+
+    public static int stringMatch(String a, String b) {
+
+        // return data: int
+        // param: String
+        //figure out which string is shorter with Math.min
+
+
+        int count = 0;
+        int length = Math.min(a.length(), b.length());
+
+        for(int i = 0; i < length-1; i++) {
+            String aSub = a.substring(i, i + 2);  // look at each substring
+            String bSub = b.substring(i, i + 2);
+            if(aSub.equals(bSub)){
+                count++;
+            }
+
+        }
+        return count;
+
+
+    }
+
+
+}
+
+/*
+
+Given 2 strings, a and b, return the number of the positions where they contain the
+same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+substrings appear in the same place in both strings.
+
+stringMatch("xxcaazz", "xxbaaz") → 3
+stringMatch("abc", "abc") → 2
+stringMatch("abc", "axc") → 0
+ */
