65922edaa0ece01eb6ae2e7208393afe983d7b07
Anna <amholmes1300@gmail.com>
2021-02-21 16:22:46 -0500

    added CountHi codingbat

---
+++ src/String2/CountHi.java
@@ -0,0 +1,29 @@
+package String2;
+
+public class CountHi {
+
+    public static int countHi(String str) {
+
+        // return data: int
+        // param: String
+
+        int hiCount = 0;
+
+        for(int i = 0; i < str.length()-1; i++) {
+            if(str.substring(i, i +2).equals("hi"))
+                hiCount++;
+        }
+        return hiCount++;
+    }
+
+
+}
+
+/*
+
+Return the number of times that the string "hi" appears anywhere in the given string.
+
+countHi("abc hi ho") → 1
+countHi("ABChi hi") → 2
+countHi("hihi") → 2
+ */
