bba28ceb89c8fcdfa649314a2abc08dc1f72570d
Anna <amholmes1300@gmail.com>
2021-02-23 18:34:04 -0500

    added CatDog codingbat

---
+++ src/String2/CatDog.java
@@ -0,0 +1,33 @@
+package String2;
+
+public class CatDog {
+    public static boolean catDog(String str) {
+
+        //return data: boolean
+        //param: string
+
+        int catCount = 0;
+        int dogCount = 0;
+
+        for(int i = 0; i < str.length()-2; i++) {
+            if(str.substring(i, i + 3).equals("cat"))
+                catCount++;
+            if(str.substring(i, i + 3).equals("dog"))
+                dogCount++;
+        }
+        if(catCount == dogCount){
+            return true;
+        }
+        return false;
+    }
+
+}
+
+/*
+
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
+ */
