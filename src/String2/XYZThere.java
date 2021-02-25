44e0cc471fd2292262961d14dfc0889ba7dc5eb1
Anna <amholmes1300@gmail.com>
2021-02-25 10:20:42 -0500

    added XYZThere codingbat

---
+++ src/String2/XYZThere.java
@@ -0,0 +1,36 @@
+package String2;
+
+public class XYZThere {
+    public static boolean xyzThere(String str) {
+
+        // return data: boolean
+        // param: String
+
+        // use substring method to look for "xyx" first with for loop
+
+
+        if(str.length() >= 3 && str.substring(0, 3).equals("xyz")) {
+            return true;
+        }
+        for(int i = 1; i < str.length()-2; i++) {
+            if(str.charAt(i-1) != '.' && str.substring(i, i+3).equals("xyz"))
+                return true;
+
+        }
+
+        return false;
+
+    }
+
+
+
+}
+
+/*
+Return true if the given string contains an appearance of "xyz" where the xyz is
+not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+xyzThere("abcxyz") → true
+xyzThere("abc.xyz") → false
+xyzThere("xyz.abc") → true
+ */
