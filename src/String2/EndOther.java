f958f6810f2a4b8655b7510f008098f791cd3037
Anna <amholmes1300@gmail.com>
2021-02-25 10:18:59 -0500

    added EndOther codingbat

---
+++ src/String2/EndOther.java
@@ -0,0 +1,40 @@
+package String2;
+
+public class EndOther {
+
+    public static boolean endOther(String a, String b) {
+
+        // return data: boolean
+        // param: String
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        String large = a;
+        String small = b;
+
+        if(a.length() < b.length()) {
+            large = b;
+            small = a;
+        }
+
+        String check = large.substring(large.length() - small.length(), large.length());
+
+        return check.equals(small);
+
+    }
+
+
+
+}
+
+/*
+
+Given two strings, return true if either of the strings appears at the very
+end of the other string, ignoring upper/lower case differences (in other words,
+the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+endOther("Hiabc", "abc") → true
+endOther("AbC", "HiaBc") → true
+endOther("abc", "abXabc") → true
+ */
