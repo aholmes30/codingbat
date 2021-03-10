3126edc0a10bb6719a44c52ee22a4dcb11f06dbb
Anna <amholmes1300@gmail.com>
2021-03-10 11:12:29 -0500

    added BobThere codingbat

---
+++ src/String2/BobThere.java
@@ -0,0 +1,29 @@
+package String2;
+
+public class BobThere {
+
+    public static boolean bobThere(String str) {
+
+        // return data: boolean
+        // param: String
+
+        // use a for loop to iterate through string and look for b_b
+
+        for(int i = 0; i < str.length()-2; i++){
+            if(str.charAt(i) == 'b' && str.charAt(i+2) == 'b')
+                return true;
+        }
+        return false;
+    }
+
+}
+
+/*
+Return true if the given string contains a "bob" string, but where the middle 'o'
+char can be any char.
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+ */
+
