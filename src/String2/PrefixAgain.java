8ba69fb5cc12ae3b026a355bbcd5752fb9c7d653
Anna <amholmes1300@gmail.com>
2021-03-24 18:57:15 -0400

    added PrefixAgain codingbat

---
+++ src/String2/PrefixAgain.java
@@ -0,0 +1,36 @@
+package String2;
+
+public class PrefixAgain {
+
+    public boolean prefixAgain(String str, int n) {
+        //method name: prefixAgain
+        //method type: 4 -> return data is boolean, params are str and int n
+
+        String prefix = str.substring(0, n);
+
+        //n-1 = last index of prefix
+
+        for(int i = n; i < str.length(); i++){
+            if(n + i <= str.length()) {
+                if(prefix.equals(str.substring(i, n+i))) {
+                    //checking if first group of N chars is going to happen again within this string
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+}
+
+/*
+Given a string, consider the prefix string made of the first N chars of the string.
+Does that prefix string appear somewhere else in the string? Assume that the string
+is not empty and that N is in the range 1..str.length().
+
+prefixAgain("abXYabc", 1) → true
+prefixAgain("abXYabc", 2) → true
+prefixAgain("abXYabc", 3) → false
+ */
